92be034d1a772f29818b93d95920314703f39b35
KhanhNguyen <nguyentrungk461@gmail.com>
2025-06-07 18:17:50 +0700

    fix booking history

--- src/main/java/com/booking/bookingbackend/data/repository/BookingRepository.java
+++ src/main/java/com/booking/bookingbackend/data/repository/BookingRepository.java
@@ -19,24 +19,29 @@ public interface BookingRepository extends BaseRepository<Booking, UUID> {
 
   @Query(value = """
       SELECT BIN_TO_UUID(b.id)             AS bookingId,
-             b.check_in       AS checkIn,
-             b.check_out      AS checkOut,
-             b.adult_units    AS adults,
-             b.children_units AS children,
-             b.total_price    AS totalPrice,
-             b.status,
-             BIN_TO_UUID(u.id)             AS user_id,
-             gb.first_name    AS firstName,
-             gb.last_name     AS lastName,
-             gb.email         AS email,
-             gb.phone_number  AS phone,
-             gb.country       AS country,
-             gb.note          AS note,
-             BIN_TO_UUID(p.id)             AS propertyId,
-             p.name           AS propertyName,
-             p.image          AS propertyImage,
-             p.address        AS propertyAddress,
-             p.province       AS propertyProvince
+            b.check_in                    AS checkIn,
+            b.check_out                   AS checkOut,
+            b.adult_units                 AS adults,
+            b.children_units              AS children,
+            b.total_price                 AS totalPrice,
+            b.status,
+            BIN_TO_UUID(u.id)             AS user_id,
+            gb.first_name                 AS firstName,
+            gb.last_name                  AS lastName,
+            gb.email                      AS email,
+            gb.phone_number               AS phone,
+            gb.country                    AS country,
+            gb.note                       AS note,
+            BIN_TO_UUID(p.id)             AS propertyId,
+            p.name                        AS propertyName,
+            p.image                       AS propertyImage,
+            p.address                     AS propertyAddress,
+            p.province                    AS propertyProvince,
+            BIN_TO_UUID(pay.id)                        AS paymentId,
+            pay.status                    AS paymentStatus,
+            pay.url_image                 AS paymentImage,
+            pay.payment_method            AS paymentMethod,
+            pay.transaction_id            AS transactionId
       FROM (SELECT id,
                    check_in,
                    check_out,
@@ -50,10 +55,11 @@ public interface BookingRepository extends BaseRepository<Booking, UUID> {
             FROM tbl_booking
             WHERE user_id = :user_id
             ORDER BY created_at DESC) AS b
-               INNER JOIN (SELECT id FROM tbl_user WHERE id = :user_id) AS u
+            INNER JOIN (SELECT id FROM tbl_user WHERE id = :user_id) AS u
                           ON b.user_id = u.id
-               INNER JOIN tbl_properties AS p ON b.properties_id = p.id
-               INNER JOIN tbl_guest_booking AS gb ON b.guest_id = gb.id
+            INNER JOIN tbl_properties AS p ON b.properties_id = p.id
+            INNER JOIN tbl_guest_booking AS gb ON b.guest_id = gb.id
+            INNER JOIN tbl_payment AS pay ON pay.booking_id = b.id
       """,
       countQuery = """
               SELECT COUNT(*)
